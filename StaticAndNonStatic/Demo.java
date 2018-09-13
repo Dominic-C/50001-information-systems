af48f118428953ff5f785659a4bfe142fd3f42d2
dominic <dominiccordeiro@gmail.com>
2018-09-13 21:15:27 +0800

    add demo code for when to use static and non static variables

--- StaticAndNonStatic/Demo.java
+++ StaticAndNonStatic/Demo.java
@@ -6,8 +6,18 @@ class A
     public void show()
     {
         num2 = 9;
+
+        
     }
+
+    public static void increment()
+    {
+        num1++;
+    }
+    // bottom line, can use static variable inside a non static method. Because all the non static members share the same static variable
+    // but cannot use non static variables inside a static method
 }
+
 
 public class Demo
 {
@@ -22,6 +32,8 @@ public class Demo
         obj1.num1 = 1;
         obj2.num2 = 2;
         obj3.num3 = 3;
-        
+
+        // if i call A.increment(), which is a static method,  we're not sure which instances of num1 are being incremented
+
     }
 }
